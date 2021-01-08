a11b71f7a0c03aa27fd944493985ff4e9bf0c897
hyelim <hohohoi914@naver.com>
2021-01-08 11:16:41 +0900

    HL nothing

--- src/main/java/kh/spring/service/BCommentService.java
+++ src/main/java/kh/spring/service/BCommentService.java
@@ -1,9 +1,12 @@
 package kh.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kh.spring.dao.BCommentDAO;
+import kh.spring.dto.BCommentDTO;
 
 @Service
 public class BCommentService {
@@ -11,4 +14,7 @@ public class BCommentService {
 	@Autowired
 	private BCommentDAO cdao;
 
+	public List<BCommentDTO> searchComment(int board_num) throws Exception{
+		return cdao.searchComment(board_num);
+	}
 }
